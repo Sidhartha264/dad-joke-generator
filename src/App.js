84ef2973ae555430328a5226c6f2a1ff9aaaa3b8
Sidhartha Bal <sidharthagautambal.2000@gmail.com>
2024-08-16 22:41:03 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+
+  const fetchDadJoke = async () => {
+    try {
+
+    const fetchJoke = await axios.get('https://icanhazdadjoke.com/', 
+    {headers: {accept: 'application/json'}})
+    setJoke(fetchJoke.data.joke);
+  
+    } catch(err) {
+      console.log('There was an Error: ', err);
+    }
+  }
+
+  const clearJoke = () => {
+    setJoke('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Dad Joke Generator</h1>
+      <button  onClick={fetchDadJoke}>Get a joke!</button>
+      <button onClick={clearJoke}>Clear</button>
+      <p>{joke}</p>
     </div>
   );
 }
